039b1803e45683d74f220750381e732bd3c96846
JonR205 <gohan205@gmail.com>
2023-11-16 14:55:45 -0600

    Added CasualtyCheckFirstRoll modal

--- src/component/Operatives.tsx
+++ src/component/Operatives.tsx
@@ -1,7 +1,28 @@
-import CasualtyCheckForstRoll from "../modals/CasualtyCheckFirstRoll"
+import { useState } from 'react'
+import CasualtyCheckFirstRoll from '../modals/CasualtyCheckFirstRoll'
+
 
 const Operatives = () => {
-  return <CasualtyCheckForstRoll />
-}
+  const [
+    showCasualtyCheckFirstRollModal,
+    setshowshowCasualtyCheckFirstRollModal,
+  ] = useState(false)
+  
+
+  return (
+    <>
+    <CasualtyCheckFirstRoll
+      showModal={showCasualtyCheckFirstRollModal}
+      onClose={() => setshowshowCasualtyCheckFirstRollModal(false)} />
+      <div className={'column'}>
+        <p className="title">Strategic Assets</p>
+        <button
+          className={'button is-primary is-small'}
+          onClick={() => setshowshowCasualtyCheckFirstRollModal(true)}>
+          Casulty Check
+        </button>
+        </div>
+      </>
+    )}
 
 export default Operatives
--- src/modals/CasualtyCheckFirstRoll.tsx
+++ src/modals/CasualtyCheckFirstRoll.tsx
@@ -1,5 +1,31 @@
-const CasualtyCheckForstRoll = () => {
-  return <div> Put Rules Here</div>
+interface Props {
+  showModal: boolean
+  onClose: () => void
 }
 
-export default CasualtyCheckForstRoll
+const CasualtyCheckFirstRoll = (props: Props) => {
+  const { showModal, onClose } = props
+
+  const isActive = showModal ? 'is-active' : ''
+
+  return (
+    <div className={`modal ${isActive}`}>
+      <button className="delete" onClick={onClose} />
+      <div className="modal-background" />
+
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title"> put rules here </p>
+          <button className="delete" onClick={onClose}></button>
+        </header>
+        <footer className="modal-card-foot">
+          <button className="button" onClick={onClose}>
+            Cancel
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default CasualtyCheckFirstRoll
