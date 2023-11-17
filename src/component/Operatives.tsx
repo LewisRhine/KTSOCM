1186814a1e3256783b25504c41366a88aa7ec38f
JonR205 <gohan205@gmail.com>
2023-11-17 15:02:11 -0600

    finished first roll modal started on second

--- src/component/Operatives.tsx
+++ src/component/Operatives.tsx
@@ -1,28 +1,33 @@
 import { useState } from 'react'
 import CasualtyCheckFirstRoll from '../modals/CasualtyCheckFirstRoll'
 
-
 const Operatives = () => {
   const [
     showCasualtyCheckFirstRollModal,
     setshowshowCasualtyCheckFirstRollModal,
   ] = useState(false)
-  
+  const [
+    showCasualtyCheckSecondRollModal,
+    setshowCasualtyCheckSecondRollModal,
+  ] = useState(false)
 
   return (
     <>
-    <CasualtyCheckFirstRoll
-      showModal={showCasualtyCheckFirstRollModal}
-      onClose={() => setshowshowCasualtyCheckFirstRollModal(false)} />
+      <CasualtyCheckFirstRoll
+        showModal={showCasualtyCheckFirstRollModal}
+        onClose={() => setshowshowCasualtyCheckFirstRollModal(false)}
+      />
       <div className={'column'}>
-        <p className="title">Strategic Assets</p>
+        <p className="title">Casulty Test Walkthrough</p>
+
         <button
           className={'button is-primary is-small'}
           onClick={() => setshowshowCasualtyCheckFirstRollModal(true)}>
           Casulty Check
         </button>
-        </div>
-      </>
-    )}
+      </div>
+    </>
+  )
+}
 
 export default Operatives
--- src/modals/CasualtyCheckFirstRoll.tsx
+++ src/modals/CasualtyCheckFirstRoll.tsx
@@ -15,13 +15,27 @@ const CasualtyCheckFirstRoll = (props: Props) => {
 
       <div className="modal-card">
         <header className="modal-card-head">
-          <p className="modal-card-title"> put rules here </p>
-          <button className="delete" onClick={onClose}></button>
+          <p className="modal-card-title"> First Roll</p>
         </header>
+        <div className="modal-card-body">
+          <p>
+            At the end of each battle, a Casualty test must be taken for each
+            friendly operative that was incapacitated in that battle. To do so,
+            roll one D6: on a 2+, the test is passed, but that operative can
+            gain no more than 3XP from that battle. On a 1, the test is failed,
+            and that operative gains a Battle Scar.
+          </p>
+          <p>
+            For each friendly MEDIC operative that was selected for deployment,
+            so long as it was not incapacitated during that battle, you can
+            re-roll one Casualty test after the battle.
+          </p>
+        </div>
         <footer className="modal-card-foot">
           <button className="button" onClick={onClose}>
-            Cancel
+            Passed
           </button>
+          <button></button>
         </footer>
       </div>
     </div>
