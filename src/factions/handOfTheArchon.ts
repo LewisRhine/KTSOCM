a1ec4e2315a2c7fc9e8a044961606b5ccefcb7a0
JonR205 <gohan205@gmail.com>
2023-12-20 20:52:56 -0600

    Added Hand of the Archon faction file

---
+++ src/factions/handOfTheArchon.ts
@@ -0,0 +1,27 @@
+import { Faction } from '../data/faction.ts'
+import { Gear, Weapon } from '../data/equipment.ts'
+import { blast, indirect, lethal, limited, rng } from '../data/specialRules.ts'
+import { StrategicAssets } from '../data/strategicAssets.ts'
+import { Requisition } from '../data/requisition.ts'
+
+
+
+
+
+
+
+
+export const handOfTheArchon: Faction = {
+    id: 2,
+    name: 'Hand Of The Archon',
+    keyword: 'HAND OF THE ARCHON',
+    coverImage: '',
+    historyTable: [],
+    quirkTable: [],
+    equipment: [],
+    rareEquipment: [],
+    strategicAssets: [],
+    requisitions: [],
+    specOps: []
+  }
+  
