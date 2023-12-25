1360fdc53f1e598854c832651c01db939312c964
JonR205 <gohan205@gmail.com>
2023-12-24 19:02:34 -0600

    learning

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = (props: Props) => {
         className={`navbar-menu ${burgerIsExpanded ? 'is-active' : ''}`}>
         <div className="navbar-start">
           <a className="navbar-item" onClick={() => nav('/')}>
-            Dashboard slash only
+            Dashboard
           </a>
         </div>
         <div className="navbar-end">
